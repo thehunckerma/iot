dbede50d2143293ed4239351697a25d419216bd7
thehunckerma <thehunckerma@hotmail.com>
2021-02-03 12:40:54 +0100

    Add opencv detection and websockets demos

---
+++ README.md
@@ -0,0 +1,23 @@
+# IOT de merde
+
+### Setup conda env
+
+```
+conda create -n iot python=3
+conda activate iot
+conda install -c conda-forge fastapi
+conda install -c conda-forge uvicorn
+conda install -c conda-forge opencv=4.1.0
+```
+
+# Body/face detection
+
+```
+python detect.py
+```
+
+# Websockets
+
+```
+uvicorn api:api --reload
+```
---
+++ api.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+import uvicorn
+
+# ======================================================================
+# ========================== Web Sockets Demo ==========================
+# ======================================================================
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+api = FastAPI()
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@api.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@api.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
+if __name__ == '__main__':
+    uvicorn.run(api, host='127.0.0.1', port=8080)
