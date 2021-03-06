4d5fdd0bc53cf25ed932afc4608347a836034979
thehunckerma <thehunckerma@hotmail.com>
2021-03-06 16:17:45 +0100

    bruh

--- main/main.ino
+++ main/main.ino
@@ -76,14 +76,36 @@ void setup()
 
     sensor_t *s = esp_camera_sensor_get();
     //initial sensors are flipped vertically and colors are a bit saturated
-    if (s->id.PID == OV3660_PID)
-    {
-        s->set_vflip(s, 1);       //flip it back
-        s->set_brightness(s, 1);  //up the blightness just a bit
-        s->set_saturation(s, -2); //lower the saturation
-    }
+    // if (s->id.PID == OV3660_PID)
+    // {
+    //     s->set_vflip(s, 1);       //flip it back
+    //     s->set_brightness(s, 1);                 //up the blightness just a bit
+    //     s->set_saturation(s, -2);                //lower the saturation
+    // }
     //drop down frame size for higher initial frame rate
     s->set_framesize(s, FRAMESIZE_QVGA);
+    s->set_brightness(s, 0);                 // -2 to 2
+    s->set_contrast(s, 0);                   // -2 to 2
+    s->set_saturation(s, 0);                 // -2 to 2
+    s->set_special_effect(s, 0);             // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+    s->set_whitebal(s, 1);                   // 0 = disable , 1 = enable
+    s->set_awb_gain(s, 1);                   // 0 = disable , 1 = enable
+    s->set_wb_mode(s, 0);                    // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+    s->set_exposure_ctrl(s, 1);              // 0 = disable , 1 = enable
+    s->set_aec2(s, 0);                       // 0 = disable , 1 = enable
+    s->set_ae_level(s, 0);                   // -2 to 2
+    s->set_aec_value(s, 300);                // 0 to 1200
+    s->set_gain_ctrl(s, 1);                  // 0 = disable , 1 = enable
+    s->set_agc_gain(s, 0);                   // 0 to 30
+    s->set_gainceiling(s, (gainceiling_t)0); // 0 to 6
+    s->set_bpc(s, 0);                        // 0 = disable , 1 = enable
+    s->set_wpc(s, 1);                        // 0 = disable , 1 = enable
+    s->set_raw_gma(s, 1);                    // 0 = disable , 1 = enable
+    s->set_lenc(s, 1);                       // 0 = disable , 1 = enable
+    s->set_hmirror(s, 0);                    // 0 = disable , 1 = enable
+    s->set_vflip(s, 0);                      // 0 = disable , 1 = enable
+    s->set_dcw(s, 1);                        // 0 = disable , 1 = enable
+    s->set_colorbar(s, 0);                   // 0 = disable , 1 = enable
 
 #if defined(CAMERA_MODEL_M5STACK_WIDE)
     s->set_vflip(s, 1);
